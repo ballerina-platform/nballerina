190f549beafdca328dfeca8b032e21fd90f13176
James Clark <jjc@jclark.com>
2021-05-27 08:37:45 +0700

    Create LLVM module

--- build.bat
+++ build.bat
@@ -1,10 +1,13 @@
-cd bir
+cd types
 call bal build -c
 call bal push --repository=local
-cd ..\types
+cd ..\bir
 call bal build -c --skip-tests
 call bal push --repository=local
 cd ..\front
+call bal build -c  --skip-tests
+call bal push --repository=local
+cd ..\llvm
 call bal build -c  --skip-tests
 call bal push --repository=local
 cd ..\driver
---
+++ llvm/llvm.bal
@@ -0,0 +1,8 @@
+import wso2/nballerina.bir;
+
+# The preferred output extension for the output filename.
+public const OUTPUT_EXTENSION = ".ll";
+
+public function compileModule(bir:Module module, string outputFilename) returns error? {
+    return error("sorry, not implemented yet");
+}
