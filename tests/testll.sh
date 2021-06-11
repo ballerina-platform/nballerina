17332bdf042986260eb336d8bb763169e958860e
James Clark <jjc@jclark.com>
2021-06-11 10:30:17 +0700

    Move test.c into new runtime directory

--- tests/testll.sh
+++ tests/testll.sh
@@ -4,7 +4,7 @@ for f in V*.ll
 do
     b=`basename $f .ll`
     sed -ne 's;^.* // *@output  *;;p' $b.bal | tr -d '\r' > $b-expect.txt
-    clang-11 -Wno-override-module $f test.c && ./a.out > $b-actual.txt
+    clang-11 -Wno-override-module $f ../runtime/balrt.c && ./a.out > $b-actual.txt
     diff $b-expect.txt $b-actual.txt >$b-diff.txt || echo $b failed
 done
 
