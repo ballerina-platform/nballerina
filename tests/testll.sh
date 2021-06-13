592808592b06784eeaaeda5980b36d824ef21138
manuranga perera <manu@wso2.com>
2021-06-13 19:27:37 +0530

    Move build artifacts to a separate directory

--- tests/testll.sh
+++ tests/testll.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # This expects to be run on Linux
-for f in V*.ll
+tests=`dirname "$0"`
+t="$tests/target"
+mkdir -p "$t"
+for f in "$tests"/ll/V*.ll
 do
-    b=`basename $f .ll`
-    sed -ne 's;^.* // *@output  *;;p' $b.bal | tr -d '\r' > $b-expect.txt
-    clang-11 -Wno-override-module $f ../runtime/balrt.c && ./a.out > $b-actual.txt
-    diff $b-expect.txt $b-actual.txt >$b-diff.txt || echo $b failed
+    b=`basename "$f" .ll`
+    sed -ne 's;^.* // *@output  *;;p' "$tests/$b.bal" | tr -d '\r' > "$t/$b-expect.txt"
+    clang-11 -Wno-override-module "$f" "$tests/../runtime/balrt.c" -o "$t/$b.out"  && "$t/$b.out" > "$t/$b-actual.txt"
+    diff "$t/$b-expect.txt" "$t/$b-actual.txt" >"$t/$b-diff.txt" || echo $b failed
 done
