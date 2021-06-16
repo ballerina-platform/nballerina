4ec4e37652b024bf408ba440a3699adcd9912682
James Clark <jjc@jclark.com>
2021-06-16 08:54:21 +0700

    Move balc.bat

--- balc.bat
+++
@@ -1,1 +0,0 @@
-"%BALLERINA_HOME%\dependencies\jdk-11.0.8+10-jre\bin\java" -jar compiler\target\bin\nballerina.jar %1
---
+++ compiler/balc.bat
@@ -0,0 +1,1 @@
+"%BALLERINA_HOME%\dependencies\jdk-11.0.8+10-jre\bin\java" -jar target\bin\nballerina.jar %1
