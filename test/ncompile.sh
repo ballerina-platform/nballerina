e3e802cab86a974f3170ab489bd0d8263a89ab6f
James Clark <jjc@jclark.com>
2021-06-20 05:53:25 +0700

    Add command just to compile new files

---
+++ test/ncompile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+JAVA_COMMAND=`./findJava.sh`
+new=""
+for f in ../compiler/testSuite/[VP]*.bal; do
+    ll=`basename $f .bal`.ll
+    test -f ll/$ll || new="$new $f"
+done
+$JAVA_COMMAND -jar ../compiler/target/bin/nballerina.jar --outDir . $new
