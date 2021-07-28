39f248859e3f1f98f28000534c26b8b260a6a32b
Heshan Padmasiri <heshanp@wso2.com>
2021-07-28 14:20:28 +0530

    Move string tests to seperate dir

--- runtime/tests/strings/utils.h
+++ runtime/tests/strings/utils.h
@@ -1,9 +1,9 @@
-#include "../balrt.h"
+#include "../../balrt.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
-#include "../hash.h"
+#include "../../hash.h"
 
 #define NRANDOM 2
 #define NTESTS 2*1024
