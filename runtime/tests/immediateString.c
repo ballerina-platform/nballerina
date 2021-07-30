a4996d26a91d39c889de0d7ec5fb655be72b4096
Heshan Padmasiri <heshanp@wso2.com>
2021-07-30 16:30:50 +0530

    Fix immediateStringLength test

--- runtime/tests/immediateString.c
+++ runtime/tests/immediateString.c
@@ -2,9 +2,9 @@
 
 HASH_DEFINE_KEY;
 
-void validateImmediateStringLength(uint64_t bits, int nChars, int nCodePoints) {
+void validateImmediateStringLength(uint64_t bits, int nBytes, int nCodePoints) {
 	StringLength length = immediateStringLength(bits);
-	assert(length.nBytes == nChars);
+	assert(length.nBytes == nBytes);
 	assert(length.nCodePoints == nCodePoints);
 	StringLength taggedLength = taggedStringLength(bitsToTaggedPtr(bits));
 	assert(taggedLength.nBytes == length.nBytes);
@@ -20,29 +20,33 @@ void validateTaggedStringEquals(uint64_t lhs, uint64_t rhs, bool expected) {
 
 void testImmediateStringLength() {
 	// test ASCII
-	validateImmediateStringLength(0xABFFFFFFFFFFFFFF, 0, 0); // ""
-	validateImmediateStringLength(0xABFFFFFFFFFF4142, 2, 2); // "AB"
-	validateImmediateStringLength(0xABFFFFFFFF613223, 3, 3); // "a2#"
-	validateImmediateStringLength(0xABFFFFFF256C6420, 4, 4); // "%ld "
-	validateImmediateStringLength(0xABFFFF6162606364, 5, 5); // "ab`cd"
-	validateImmediateStringLength(0xABFF2A2625242C3F, 6, 6); // "*&%$,?"
-	validateImmediateStringLength(0xAB61626364656667, 7, 7); // "abcdefg"
+	validateImmediateStringLength(0x2AFFFFFFFFFFFFFF, 0, 0); // ""
+	validateImmediateStringLength(0x2AFFFFFFFFFF4142, 2, 2); // "BA"
+	validateImmediateStringLength(0x2AFFFFFFFF613223, 3, 3); // "#2a"
+	validateImmediateStringLength(0x2AFFFFFF256C6420, 4, 4); // " dl%"
+	validateImmediateStringLength(0x2AFFFF6162606364, 5, 5); // "cd`ba"
+	validateImmediateStringLength(0x2AFF2A2625242C3F, 6, 6); // "?,$%&*"
+	validateImmediateStringLength(0x2A61626364656667, 7, 7); // "gfedcab"
 	// testUnicode 
-	// validateImmediateStringLength(0xABFFFFFFFFFF265E, 2, 1); // "♞" //! : codePoints -> 2
-	// validateImmediateStringLength(0xABFFFFFFFF01F917, 3, 1); // "🤗" //! : codePointes -> 3
-	// validateImmediateStringLength(0xABFFFFFFFF01F980, 3, 1); // "🦀" //! : nBytes -> 1
+	validateImmediateStringLength(0x2AFFFFFFFF9E99E2, 3, 1); // "♞"
+	validateImmediateStringLength(0x2AFFFFFF97A49FF0, 4, 1); // "🤗"
+	validateImmediateStringLength(0x2AFFFFFF90A69FF0, 4, 1); // "🦀"
 }
 
 void testTaggedStringEquals() {
 	// test ASCII
-	validateTaggedStringEquals(0xABFFFF6162606364, 0xABFFFF6162606364, true);
-	validateTaggedStringEquals(0xABFFFF6162606364, 0xAB61626364656667, false);
-	validateTaggedStringEquals(0xABFFFF6162606364, 0xABFFFFFFFFFF265E, false);
+	validateTaggedStringEquals(0x2AFFFF6162606364, 0x2AFFFF6162606364, true); // ASCII - ASCII
+	validateTaggedStringEquals(0x2AFFFF6162606364, 0x2A61626364656667, false); // ASCII - ASCII
+	validateTaggedStringEquals(0x2AFFFF6162606364, 0x2AFFFFFFFFFF265E, false); // ASCII - UNICODE
 	// test Unicode
-	validateTaggedStringEquals(0xABFFFFFFFFFF265E, 0xABFFFFFFFFFF265E, true);
-	validateTaggedStringEquals(0xABFFFFFFFFFF265E, 0xABFFFFFFFF01F980, false);
-	validateTaggedStringEquals(0xABFFFFFFFFFF265E, 0xABFFFF6162606364, false);
-	validateTaggedStringEquals(0x8FFFFFFFFFFF265E, 0xABFFFF6162606364, false);
+	validateTaggedStringEquals(0x2AFFFFFFFF9E99E2, 0x2AFFFFFFFF9E99E2, true); // UNICODE - UNICODE
+	validateTaggedStringEquals(0x2AFFFFFFFF9E99E2, 0x2AFFFFFF97A49FF0, false); // UNICODE - UNICODE
+	validateTaggedStringEquals(0x2AFFFFFFFF9E99E2, 0x2AFFFF6162606364, false); // UNICODE - ASCII
+	// One not immediate
+	validateTaggedStringEquals(0x8FFFFFFFFF9E99E2, 0x2AFFFFFFFF9E99E2, false); // NON-IMMEDIATE - UNICODE
+	validateTaggedStringEquals(0x8FFFFFFFFF9E99E2, 0x2AFFFF6162606364, false); // NON-IMMEDIATE - ASCII
+	validateTaggedStringEquals(0x2AFFFFFFFF9E99E2, 0x8FFFFFFFFF9E99E2, false); // UNICODE - NON-IMMEDIATE
+	validateTaggedStringEquals(0x2AFFFF6162606364, 0x8FFFFFFFFF9E99E2, false); // ASCII - NON-IMMEDIATE
 }
 
 int main() {
