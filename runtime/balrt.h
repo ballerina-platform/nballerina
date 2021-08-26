c2c99069484c656d64e1b41a76e75b3442894c28
James Clark <jjc@jclark.com>
2021-08-26 14:34:30 +0700

    Rework runtime use of ptrmask intrinsic
    Fixes up #201

--- runtime/balrt.h
+++ runtime/balrt.h
@@ -185,7 +185,7 @@ extern TaggedPtr _bal_mapping_construct(int64_t capacity);
 extern void _bal_mapping_init_member(TaggedPtr mapping, TaggedPtr key, TaggedPtr val);
 extern Error _bal_mapping_set(TaggedPtr mapping, TaggedPtr key, TaggedPtr val);
 extern READONLY TaggedPtr _bal_mapping_get(TaggedPtr mapping, TaggedPtr key);
-extern READNONE UntypedPtr taggedToPtr(TaggedPtr p);
+extern READNONE UntypedPtr _bal_tagged_to_ptr(TaggedPtr p);
 
 static READNONE inline uint64_t taggedPtrBits(TaggedPtr p) {
     return (uint64_t)(char *)p;
@@ -202,6 +202,10 @@ static READNONE inline int getTag(TaggedPtr p) {
 
 static READNONE inline int taggedToBoolean(TaggedPtr p) {
     return (int)(taggedPtrBits(p) & 1);
+}
+
+static READNONE inline UntypedPtr taggedToPtr(TaggedPtr p) {
+    return _bal_tagged_to_ptr(p);
 }
 
 static READONLY inline int64_t taggedToInt(TaggedPtr p) {
--- runtime/tagged_inline.ll
+++ runtime/tagged_inline.ll
@@ -1,6 +1,6 @@
 declare i8 addrspace(1)* @llvm.ptrmask.p1i8.i64(i8 addrspace(1)*, i64) readnone speculatable
 
-define dso_local i8 addrspace(1)* @taggedToPtr(i8 addrspace(1)* %0) alwaysinline {
+define i8 addrspace(1)* @_bal_tagged_to_ptr(i8 addrspace(1)* %0) alwaysinline {
   %2 = call i8 addrspace(1)* @llvm.ptrmask.p1i8.i64(i8 addrspace(1)* %0, i64 72057594037927928)
   ret i8 addrspace(1)* %2
 }
