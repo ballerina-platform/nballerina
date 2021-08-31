b397033febde3698e7a4c31f52e0e79c09cfe655
Kavindu Gimhan Zoysa <kavindu175@gmail.com>
2021-09-01 00:39:11 +0530

    Fix review suggestions

--- runtime/error.c
+++ runtime/error.c
@@ -5,111 +5,133 @@
 #include "balrt.h"
 #include "third-party/libbacktrace/backtrace.h"
 
-#define MIN_FRAME_COUNT 2
+#define INITIAL_FRAME_COUNT 1
+#define MAX_FRAME_COUNT 200
+#define MAX_BYTES_COUNT INT32_MAX
+#define INITIAL_BYTES_COUNT 8
 #define SKIP_FROM_END 1 // This number counts from the last stack frame
 #define THREAD 0
 
 typedef struct {
     uint32_t nFrames;
-    uint32_t capacityOfFrames;
+    uint32_t cFrames;
     uint32_t nBytes;
-    uint32_t capacityOfBytes;
-    Frame frames[];
+    uint32_t cBytes;
+    Frame *frames;
+    char *bytes;
 } FramesInfo;
 
-void on_error(void *vdata, const char *msg, int errnum) { printf("Error : %s\n", msg); }
+static void *memAlloc(uint64_t nBytes) {
+    void *p = malloc(nBytes);
+    if (p != 0)
+        return p;
+    fprintf(stderr, "out of memory\n");
+    fflush(stderr);
+    abort();    
+}
 
-int on_frame(void *data, uintptr_t pc, const char *filename, int lineno, const char *function) {
+static void *memReAlloc(void *ptr, uint64_t nBytes) {
+    void *p = realloc(ptr, nBytes);
+    if (p != 0) {
+        return p;
+    }
+    fprintf(stderr, "out of memory\n");
+    fflush(stderr);
+    abort();
+}
+
+static void on_error(void *data, const char *msg, int errnum) { 
+    fprintf(stderr, "Error : %s\n", msg);
+    fflush(stderr);
+    abort();
+}
+
+static int on_frame(void *data, uintptr_t pc, const char *filename, int lineno, const char *function) {
     // This check is valid if there is a posibility not to have debug info
-    if (filename == NULL || function == NULL)
-    {
+    if (filename == NULL || function == NULL) {
         return 0;
     }
-
-    FramesInfo *framesInfo = (FramesInfo *)data;
-    Frame *frames = framesInfo->frames;
-    uint32_t nFrames = framesInfo->nFrames;
-    uint32_t capacityOfFrames = framesInfo->capacityOfFrames;
-    uint32_t nBytes = framesInfo->nBytes;
-    uint32_t capacityOfBytes = framesInfo->capacityOfBytes;
-
-    uint64_t filenameLen = strlen(filename);
-    uint64_t funcLen = strlen(function);
-
-    if (nFrames == capacityOfFrames || (nBytes + filenameLen + funcLen + 2) >= capacityOfBytes)
-    {
-        // The current total memory size of bytes array and frames array is multiplied by two.
-        // New allocation should be devided equally between bytes array and frames array.
-        uint32_t newSize = sizeof(FramesInfo) + ((capacityOfFrames * sizeof(Frame) + capacityOfBytes) << 1);
-        framesInfo = realloc(framesInfo, newSize);
-
-        uint32_t newCapacityOfFrames = capacityOfFrames << 1;
-        memcpy(frames + newCapacityOfFrames, frames + capacityOfFrames, nBytes);
-        framesInfo->capacityOfFrames = newCapacityOfFrames;
-        framesInfo->capacityOfBytes = capacityOfBytes << 1;
-        capacityOfFrames = newCapacityOfFrames;
+    FramesInfo *info = (FramesInfo *)data;
+    uint32_t nFrames = info->nFrames;
+    uint32_t cFrames = info->cFrames;
+    if (nFrames >= MAX_FRAME_COUNT) {
+        fprintf(stderr, "reach maximum frame count\n");
+        fflush(stderr);
+        abort();
+    }
+    else if (nFrames == cFrames) {
+        cFrames = cFrames * 2;
+        info->cFrames = cFrames;
+        info->frames = (void *)memReAlloc(info->frames, sizeof(Frame) * cFrames);
     }
 
-    Frame *frame = frames + nFrames;
+    uint32_t nBytes = info->nBytes;
+    uint32_t cBytes = info->cBytes;
+    uint64_t filenameLen = strlen(filename);
+    uint64_t funcNameLen = strlen(function);
+    uint32_t nBytesPerFrame = filenameLen + funcNameLen + 2;
+    if (nBytes + nBytesPerFrame >= MAX_BYTES_COUNT) {
+        fprintf(stderr, "reach maximum character array length\n");
+        fflush(stderr);
+        abort();
+    }
+    else if (nBytes + nBytesPerFrame > cBytes) {
+        cBytes = cBytes * 2 + nBytesPerFrame;
+        info->cBytes = cBytes;
+        info->bytes = (void *)memReAlloc(info->bytes, cBytes);   
+    }
+
+    Frame *frame = info->frames + nFrames;
     frame->pc = pc;
     frame->lineno = lineno;
 
-    char *bytes = (char *)(frames + capacityOfFrames) + nBytes;
-
-    memcpy(bytes, filename, filenameLen);
+    char *bytes = info->bytes;
     frame->fileOffset = nBytes;
-    bytes = bytes + filenameLen;
+    memcpy(bytes + nBytes, filename, filenameLen);
     nBytes = nBytes + filenameLen;
-    *bytes = '\0';
-    bytes++;
+    *(bytes + nBytes) = '\0';
     nBytes++;
 
-    memcpy(bytes, function, funcLen);
     frame->functionOffset = nBytes;
-    bytes = bytes + funcLen;
-    nBytes = nBytes + funcLen;
-    *bytes = '\0';
-    bytes++;
+    memcpy(bytes + nBytes, function, funcNameLen);
+    nBytes = nBytes + funcNameLen;
+    *(bytes + nBytes) = '\0';
     nBytes++;
 
-    framesInfo->nBytes = nBytes;
-    framesInfo->nFrames = nFrames + 1;
+    info->nBytes = nBytes;
+    info->nFrames = nFrames + 1;
 
     return 0;
 }
 
-FramesInfo *getFramesInfo() {
+static void getFramesInfo(FramesInfo* info) {
     struct backtrace_state *state = backtrace_create_state(NULL, THREAD, on_error, NULL);
 
-    // The memory size of bytes array and frames array are equal
-    // Therefore minimum frame size is multiplied by 2
-    FramesInfo *framesInfo = malloc(sizeof(FramesInfo) + MIN_FRAME_COUNT * sizeof(Frame) * 2);
-    framesInfo->capacityOfFrames = MIN_FRAME_COUNT;
-    framesInfo->nFrames = 0;
-    framesInfo->capacityOfBytes = MIN_FRAME_COUNT * sizeof(Frame);
-    framesInfo->nBytes = 0;
+    info->frames = (Frame *)memAlloc(sizeof(Frame) * INITIAL_FRAME_COUNT);
+    info->bytes = (char *)memAlloc(INITIAL_BYTES_COUNT);
 
-    backtrace_full(state, SKIP_FROM_END, on_frame, on_error, framesInfo);
-    return framesInfo;
+    backtrace_full(state, SKIP_FROM_END, on_frame, on_error, info);
 }
 
 TaggedPtr _bal_error_construct(TaggedPtr message, int64_t lineNumber) {
-    FramesInfo *framesInfo = getFramesInfo();
-    uint32_t nFrames = framesInfo->nFrames;
-    uint32_t nBytes = framesInfo->nBytes;
-    Frame *frames = framesInfo->frames;
+    FramesInfo info = {0, INITIAL_FRAME_COUNT, 0, INITIAL_BYTES_COUNT};
+    getFramesInfo(&info);
+    uint32_t nFrames = info.nFrames;
+    uint32_t nBytes = info.nBytes;
 
     uint64_t errStructSize = sizeof(struct Error) + sizeof(Frame) * nFrames + nBytes;
     ErrorPtr ep = _bal_alloc(errStructSize);
-
     ep->message = message;
     ep->lineNumber = lineNumber;
     ep->nFrames = nFrames;
-    // Copy the frames
+
+    Frame *frames = info.frames;
+    char *bytes = info.bytes;
     memcpy(ep->frames, frames, sizeof(Frame) * nFrames);
-    // Copy bytes
-    memcpy(ep->frames + nFrames, frames + framesInfo->capacityOfFrames, nBytes);
-    free(framesInfo);
+    memcpy(ep->frames + nFrames, bytes, nBytes);
+
+    free(frames);
+    free(bytes);
 
     return ptrAddFlags(ep, (uint64_t)TAG_ERROR << TAG_SHIFT);
 }
