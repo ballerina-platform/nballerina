b7fe07ea5c6e22a262ca7fceb77f8cf2933df015
Kavindu Gimhan Zoysa <kavindu175@gmail.com>
2021-09-02 19:44:27 +0530

    Fix few more suggestions

--- runtime/error.c
+++ runtime/error.c
@@ -7,19 +7,47 @@
 
 #define INITIAL_FRAME_COUNT 1
 #define MAX_FRAME_COUNT 200
-#define MAX_BYTES_COUNT INT32_MAX
+#define MAX_BYTES_COUNT INT32_MAX / 10
 #define INITIAL_BYTES_COUNT 8
+#define BACKWARD_FRAMES_COUNT 4
 #define SKIP_FROM_END 1 // This number counts from the last stack frame
 #define THREAD 0
 
+struct backtrace_state *state;
+
 typedef struct {
     uint32_t nFrames;
-    uint32_t cFrames;
+    uint32_t szFrames;
     uint32_t nBytes;
-    uint32_t cBytes;
+    uint32_t szBytes;
     Frame *frames;
     char *bytes;
-} FramesInfo;
+} Trace;
+
+typedef struct {
+    int fileOffset;
+    int functionOffset;
+} Offset;
+
+void checkForSameStr(Frame *frame, uint32_t nFrames, const char *filename, const char *function, char *bytes, Offset* result) {
+    int i = 0;
+    bool filenameFound = false;
+    bool functionFound = false;
+    while (i < nFrames && i < BACKWARD_FRAMES_COUNT) {
+        frame = frame - i++;
+        if (!filenameFound && strcmp(filename, bytes + frame->fileOffset) == 0) {
+            result->fileOffset = frame->fileOffset;
+            filenameFound = true;
+        }
+        if (!functionFound && strcmp(function, bytes + frame->functionOffset) == 0) {
+            result->functionOffset = frame->functionOffset;
+            functionFound = true;
+        }
+        if (filenameFound && functionFound) {
+            return;
+        }
+    }
+}
 
 static void *memAlloc(uint64_t nBytes) {
     void *p = malloc(nBytes);
@@ -40,84 +68,95 @@ static void *memReAlloc(void *ptr, uint64_t nBytes) {
     abort();
 }
 
-static void on_error(void *data, const char *msg, int errnum) { 
+static void onError(void *data, const char *msg, int errnum) { 
     fprintf(stderr, "Error : %s\n", msg);
     fflush(stderr);
     abort();
 }
 
-static int on_frame(void *data, uintptr_t pc, const char *filename, int lineno, const char *function) {
+static int onFrame(void *data, uintptr_t pc, const char *filename, int lineno, const char *function) {
     // This check is valid if there is a posibility not to have debug info
     if (filename == NULL || function == NULL) {
         return 0;
     }
-    FramesInfo *info = (FramesInfo *)data;
-    uint32_t nFrames = info->nFrames;
-    uint32_t cFrames = info->cFrames;
+    Trace *trace = (Trace *)data;
+    uint32_t nFrames = trace->nFrames;
+    uint32_t szFrames = trace->szFrames;
     if (nFrames >= MAX_FRAME_COUNT) {
         fprintf(stderr, "reach maximum frame count\n");
         fflush(stderr);
         abort();
     }
-    else if (nFrames == cFrames) {
-        cFrames = cFrames * 2;
-        info->cFrames = cFrames;
-        info->frames = (void *)memReAlloc(info->frames, sizeof(Frame) * cFrames);
+    else if (nFrames == szFrames) {
+        szFrames = szFrames * 2;
+        trace->szFrames = szFrames;
+        trace->frames = (void *)memReAlloc(trace->frames, sizeof(Frame) * szFrames);
     }
 
-    uint32_t nBytes = info->nBytes;
-    uint32_t cBytes = info->cBytes;
+    uint32_t nBytes = trace->nBytes;
+    uint32_t szBytes = trace->szBytes;
     uint64_t filenameLen = strlen(filename);
-    uint64_t funcNameLen = strlen(function);
-    uint32_t nBytesPerFrame = filenameLen + funcNameLen + 2;
+    uint64_t funcnameLen = strlen(function);
+    uint32_t nBytesPerFrame = filenameLen + funcnameLen + 2;
     if (nBytes + nBytesPerFrame >= MAX_BYTES_COUNT) {
         fprintf(stderr, "reach maximum character array length\n");
         fflush(stderr);
         abort();
     }
-    else if (nBytes + nBytesPerFrame > cBytes) {
-        cBytes = cBytes * 2 + nBytesPerFrame;
-        info->cBytes = cBytes;
-        info->bytes = (void *)memReAlloc(info->bytes, cBytes);   
+    else if (nBytes + nBytesPerFrame > szBytes) {
+        szBytes = szBytes * 2 + nBytesPerFrame;
+        trace->szBytes = szBytes;
+        trace->bytes = (void *)memReAlloc(trace->bytes, szBytes);   
     }
 
-    Frame *frame = info->frames + nFrames;
+    Frame *frame = trace->frames + nFrames;
     frame->pc = pc;
     frame->lineno = lineno;
 
-    char *bytes = info->bytes;
-    frame->fileOffset = nBytes;
-    memcpy(bytes + nBytes, filename, filenameLen);
-    nBytes = nBytes + filenameLen;
-    *(bytes + nBytes) = '\0';
-    nBytes++;
+    Offset offset = {-1, -1};
+    char *bytes = trace->bytes;
+    checkForSameStr(frame - 1, nFrames, filename, function, bytes, &offset);
 
-    frame->functionOffset = nBytes;
-    memcpy(bytes + nBytes, function, funcNameLen);
-    nBytes = nBytes + funcNameLen;
-    *(bytes + nBytes) = '\0';
-    nBytes++;
+    if (offset.fileOffset >= 0) {
+        frame->fileOffset = offset.fileOffset;
+    } else {
+        frame->fileOffset = nBytes;
+        memcpy(bytes + nBytes, filename, filenameLen);
+        nBytes = nBytes + filenameLen;
+        *(bytes + nBytes) = '\0';
+        nBytes++;
+    }
 
-    info->nBytes = nBytes;
-    info->nFrames = nFrames + 1;
+    if (offset.functionOffset >= 0) {
+        frame->functionOffset = offset.functionOffset;
+    } else {
+        frame->functionOffset = nBytes;
+        memcpy(bytes + nBytes, function, funcnameLen);
+        nBytes = nBytes + funcnameLen;
+        *(bytes + nBytes) = '\0';
+        nBytes++;
+    }
+
+    trace->nBytes = nBytes;
+    trace->nFrames = nFrames + 1;
 
     return 0;
 }
 
-static void getFramesInfo(FramesInfo* info) {
-    struct backtrace_state *state = backtrace_create_state(NULL, THREAD, on_error, NULL);
+static void getBackTrace(Trace* trace) {
+    state = backtrace_create_state(NULL, THREAD, onError, NULL);
 
-    info->frames = (Frame *)memAlloc(sizeof(Frame) * INITIAL_FRAME_COUNT);
-    info->bytes = (char *)memAlloc(INITIAL_BYTES_COUNT);
+    trace->frames = (Frame *)memAlloc(sizeof(Frame) * INITIAL_FRAME_COUNT);
+    trace->bytes = (char *)memAlloc(INITIAL_BYTES_COUNT);
 
-    backtrace_full(state, SKIP_FROM_END, on_frame, on_error, info);
+    backtrace_full(state, SKIP_FROM_END, onFrame, onError, trace);
 }
 
 TaggedPtr _bal_error_construct(TaggedPtr message, int64_t lineNumber) {
-    FramesInfo info = {0, INITIAL_FRAME_COUNT, 0, INITIAL_BYTES_COUNT};
-    getFramesInfo(&info);
-    uint32_t nFrames = info.nFrames;
-    uint32_t nBytes = info.nBytes;
+    Trace trace = {0, INITIAL_FRAME_COUNT, 0, INITIAL_BYTES_COUNT};
+    getBackTrace(&trace);
+    uint32_t nFrames = trace.nFrames;
+    uint32_t nBytes = trace.nBytes;
 
     uint64_t errStructSize = sizeof(struct Error) + sizeof(Frame) * nFrames + nBytes;
     ErrorPtr ep = _bal_alloc(errStructSize);
@@ -125,8 +164,8 @@ TaggedPtr _bal_error_construct(TaggedPtr message, int64_t lineNumber) {
     ep->lineNumber = lineNumber;
     ep->nFrames = nFrames;
 
-    Frame *frames = info.frames;
-    char *bytes = info.bytes;
+    Frame *frames = trace.frames;
+    char *bytes = trace.bytes;
     memcpy(ep->frames, frames, sizeof(Frame) * nFrames);
     memcpy(ep->frames + nFrames, bytes, nBytes);
 
