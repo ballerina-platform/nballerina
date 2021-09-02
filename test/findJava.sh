ba796d6c2c67f2e32d6a1d2407c3f1978e53abfe
manuranga perera <manu@wso2.com>
2021-09-02 23:48:51 +0530

    Run GitHub Actions on macOS

--- test/findJava.sh
+++ test/findJava.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
 bal_command=`which bal`
-bal_command=`realpath "$bal_command"`
 bal_dir=`dirname "$bal_command"`
-jre_bin_dir=`realpath "$bal_dir/../dependencies/jdk-11.0.8+10-jre/bin"`
+jre_bin_dir="$bal_dir/../dependencies/jdk-11.0.8+10-jre/bin"
 echo "$jre_bin_dir/java"
