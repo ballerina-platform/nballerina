5d6c57e7680d1cebb931ab3b2dd672edb3b32234
Kavindu Gimhan Zoysa <kavindu175@gmail.com>
2021-11-09 18:03:41 +0530

    Compare the trace in a simple way

--- runtime/tests/error_backtrace1.sh
+++ runtime/tests/error_backtrace1.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-expectedTrace=(\
-"func1 .*/error_backtrace1.c:20" \
-"main .*/error_backtrace1.c:27" \
-"func2 .*/error_backtrace1.c:13" \
-"func1 .*/error_backtrace1.c:23" \
-"main .*/error_backtrace1.c:27" \
-"func3 .*/error_backtrace1.c:7" \
-"func2 .*/error_backtrace1.c:16" \
-"func1 .*/error_backtrace1.c:23" \
-"main .*/error_backtrace1.c:27" \
-)
-
-actualTrace=$(</dev/stdin)
-for t in "${expectedTrace[@]}"; do
-    exist=$(echo $actualTrace | grep "$t")
-    if test `expr length "$exist"` -le 0; then
-        exit 1
-    fi
-done
+diff=$(sed 's/\/.*\/error_backtrace/error_backtrace/' /dev/stdin | diff - error_backtrace1.txt)
+if grep "^---" <<< $diff; then
+    exit 1
+fi
--- runtime/tests/error_backtrace2.sh
+++ runtime/tests/error_backtrace2.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-expectedTrace=(\
-"func2 .*/error_backtrace2.c:23" \
-"func1 .*/error_backtrace2.c:15" \
-"func2 .*/error_backtrace2.c:20" \
-"func1 .*/error_backtrace2.c:15" \
-"func2 .*/error_backtrace2.c:20" \
-"func1 .*/error_backtrace2.c:15" \
-"main .*/error_backtrace2.c:30" \
-)
-
-actualTrace=$(</dev/stdin)
-for t in "${expectedTrace[@]}"; do
-    exist=$(echo $actualTrace | grep "$t")
-    if test `expr length "$exist"` -le 0; then
-        exit 1
-    fi
-done
+#!/bin/bash
+diff=$(sed 's/\/.*\/error_backtrace/error_backtrace/' /dev/stdin | diff - error_backtrace2.txt)
+if grep "^---" <<< $diff; then
+    exit 1
+fi
