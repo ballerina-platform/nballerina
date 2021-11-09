28115c05c0b229bc3202d99519db5e59d13f3836
KavinduZoysa <kavindu175@gmail.com>
2021-11-09 19:02:58 +0530

    Change runtime backtrace tests (#676)

--- runtime/tests/error_backtrace1.c
+++ runtime/tests/error_backtrace1.c
@@ -3,62 +3,23 @@
 
 HASH_DEFINE_KEY;
 
-// We have to check the function and filename seperately
-// because filepath is not a constant
-static const ExpectedTrace expectedTrace1[] = {
-    { "    func1 ", "error_backtrace1.c:56" },
-    { "    main ", "error_backtrace1.c:66" }
-};
-
-static const ExpectedTrace expectedTrace2[] = {
-    { "    func2 ", "error_backtrace1.c:46" },
-    { "    func1 ", "error_backtrace1.c:62" },
-    { "    main ", "error_backtrace1.c:66" }
-};
-
-static const ExpectedTrace expectedTrace3[] = {
-    { "    func3 ", "error_backtrace1.c:37" },
-    { "    func2 ", "error_backtrace1.c:52" },
-    { "    func1 ", "error_backtrace1.c:62" },
-    { "    main ", "error_backtrace1.c:66" }
-};
-
-bool genAndCmpTrace(ErrorPtr ep, const ExpectedTrace expectedTrace[], uint32_t traceStart) {
-    FILE *fp = tmpfile();
-    assert(fp != NULL);
-    _bal_error_backtrace_print(ep, traceStart, fp);
-    rewind(fp);
-    bool res = cmpTrace(fp, expectedTrace);
-    fclose(fp);
-    return res;    
-}
-
 void func3() {
-    TaggedPtr taggedErr = _bal_error_construct(makeString("Func3 error"), 37);
+    TaggedPtr taggedErr = _bal_error_construct(makeString("Func3 error"), 7);
     ErrorPtr ep = (ErrorPtr)taggedToPtr(taggedErr);
-
-    if (!genAndCmpTrace(ep, expectedTrace3, 1)) {
-        assert(genAndCmpTrace(ep, expectedTrace3, 2));
-    }
+    _bal_error_backtrace_print(ep, 1, stdout);
 }
 
 void func2() {
-    TaggedPtr taggedErr = _bal_error_construct(makeString("Func2 error"), 46);
+    TaggedPtr taggedErr = _bal_error_construct(makeString("Func2 error"), 13);
     ErrorPtr ep = (ErrorPtr)taggedToPtr(taggedErr);
-
-    if (!genAndCmpTrace(ep, expectedTrace2, 1)) {
-        assert(genAndCmpTrace(ep, expectedTrace2, 2));
-    }   
+    _bal_error_backtrace_print(ep, 1, stdout); 
     func3();
 }
 
 void func1() {
-    TaggedPtr taggedErr = _bal_error_construct(makeString("Func1 error"), 56);
+    TaggedPtr taggedErr = _bal_error_construct(makeString("Func1 error"), 20);
     ErrorPtr ep = (ErrorPtr)taggedToPtr(taggedErr);
-
-    if (!genAndCmpTrace(ep, expectedTrace1, 1)) {
-        assert(genAndCmpTrace(ep, expectedTrace1, 2));
-    }
+    _bal_error_backtrace_print(ep, 1, stdout);
     func2();
 }
 
--- runtime/tests/error_backtrace2.c
+++ runtime/tests/error_backtrace2.c
@@ -1,68 +1,31 @@
 #include <assert.h>
 #include <stdio.h>
-
 #include "string_utils.h"
 
 HASH_DEFINE_KEY;
 
-// We have to check the function and filename seperately
-// because filepath is not a constant
-static const ExpectedTrace expectedTrace1[] = {
-    { "    func2 ", "error_backtrace2.c:48" },
-    { "    func1 ", "error_backtrace2.c:40" },
-    { "    func2 ", "error_backtrace2.c:45" },
-    { "    func1 ", "error_backtrace2.c:40" },
-    { "    func2 ", "error_backtrace2.c:45" },
-    { "    func1 ", "error_backtrace2.c:40" },
-    { "    genAndCmpTrace ", "error_backtrace2.c:57" },
-    { "    main ", "error_backtrace2.c:65" }
-};
-
-static const ExpectedTrace expectedTrace2[] = {
-    { "    func2 ", "error_backtrace2.c:48" },
-    { "    func1 ", "error_backtrace2.c:40" },
-    { "    func2 ", "error_backtrace2.c:45" },
-    { "    func1 ", "error_backtrace2.c:40" },
-    { "    func2 ", "error_backtrace2.c:45" },
-    { "    func1 ", "error_backtrace2.c:40" },
-    { "    genAndCmpTrace ", "error_backtrace2.c:57" },
-    { "    main ", "error_backtrace2.c:66" }
-};
-
-void func2(int, FILE *fp, uint32_t traceStart);
-void func1(int, FILE *fp, uint32_t traceStart);
+void func2(int);
+void func1(int);
 
 int decrement(int i) {
     return --i;
 }
 
-void func1(int j, FILE *fp, uint32_t traceStart) {
-    func2(decrement(j), fp, traceStart);
+void func1(int j) {
+    func2(decrement(j));
 }
 
-void func2(int j, FILE *fp, uint32_t traceStart) {
+void func2(int j) {
     if (j >= 0) {
-        func1(decrement(j), fp, traceStart);
+        func1(decrement(j));
     } 
     else {
-        TaggedPtr taggedErr = _bal_error_construct(makeString("Func1 error"), 48);
+        TaggedPtr taggedErr = _bal_error_construct(makeString("Func1 error"), 23);
         ErrorPtr ep = (ErrorPtr)taggedToPtr(taggedErr);
-        _bal_error_backtrace_print(ep, traceStart, fp);
+        _bal_error_backtrace_print(ep, 1, stdout);
     }
-}
-
-bool genAndCmpTrace(uint32_t traceStart, const ExpectedTrace expectedTrace[]) {
-    FILE *fp = tmpfile();
-    assert(fp != NULL);
-    func1(3, fp, traceStart);
-    rewind(fp);
-    bool res = cmpTrace(fp, expectedTrace);
-    fclose(fp);
-    return res;
 }
 
 int main() {
-    if (!genAndCmpTrace(1, expectedTrace1)) {
-        assert(genAndCmpTrace(2, expectedTrace2));
-    }
+    func1(3);
 }
