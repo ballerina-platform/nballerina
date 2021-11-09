e5532d9d94ed173d9212248a67c3969dda042858
Kavindu Gimhan Zoysa <kavindu175@gmail.com>
2021-11-09 18:26:10 +0530

    Reduced the number of bash scripts used

--- runtime/tests/error_backtrace1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-diff=$(sed 's/\/.*\/error_backtrace/error_backtrace/' /dev/stdin | diff - error_backtrace1.txt)
-if grep "^---" <<< $diff; then
-    exit 1
-fi
--- runtime/tests/error_backtrace2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#!/bin/bash
-diff=$(sed 's/\/.*\/error_backtrace/error_backtrace/' /dev/stdin | diff - error_backtrace2.txt)
-if grep "^---" <<< $diff; then
-    exit 1
-fi
