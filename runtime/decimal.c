e27cd437bfeaad105ca8e0098bdfbd7c98a17b6d
Kavindu Gimhan Zoysa <kavindu175@gmail.com>
2021-11-12 11:15:57 +0530

    Factor out to new function

--- runtime/decimal.c
+++ runtime/decimal.c
@@ -7,21 +7,32 @@ typedef GC decQuad *DecimalPtr;
 // XXX need to investigate DEC_Underflow
 #define DECIMAL_STATUS_FAIL (DEC_Errors|DEC_Subnormal)
 
-static inline PanicCode statusPanicCode(uint32_t status) {
-    if (status & DECIMAL_STATUS_FAIL) {
-        // XXX decide what panic codes to use 
-        return (status & DEC_Division_by_zero) ? PANIC_DIVIDE_BY_ZERO : PANIC_ARITHMETIC_OVERFLOW;
-    }
-    return 0;
-}
-
 static inline TaggedPtr createDecimal(decQuad *dq) {
     DecimalPtr dp = _bal_alloc(sizeof(decQuad));
     *dp = *dq;
     return ptrAddFlags(dp, (uint64_t)TAG_DECIMAL << TAG_SHIFT);
 }
 
-static inline decQuad *taggedToDecQuad(TaggedPtr tp) {
+static inline TaggedPtrPanicCode finish(decQuad *dq, decContext *cx) {
+    TaggedPtrPanicCode result;
+    uint32_t status = cx->status;
+    if (status & DECIMAL_STATUS_FAIL) {
+        // XXX decide what panic codes to use 
+        result.panicCode = (status & DEC_Division_by_zero) ? PANIC_DIVIDE_BY_ZERO : PANIC_ARITHMETIC_OVERFLOW;
+    }
+    else {
+        result.panicCode = 0;
+    }
+    if (result.panicCode == 0) {
+        if (decQuadClass(dq) == DEC_CLASS_NEG_ZERO) {
+            decQuadZero(dq);
+        }
+        result.ptr = createDecimal(dq);
+    }
+    return result;  
+}
+
+static inline const decQuad *taggedToDecQuad(TaggedPtr tp) {
     UntypedPtr p = taggedToPtr(tp);
     return (void *)p;
 }
@@ -32,19 +43,13 @@ static inline void initContext(decContext *cx) {
 }
 
 TaggedPtrPanicCode _bal_decimal_add(TaggedPtr tp1, TaggedPtr tp2) {
-    TaggedPtrPanicCode result;
-    decQuad *dq1 = taggedToDecQuad(tp1);
-    decQuad *dq2 = taggedToDecQuad(tp2);
+    const decQuad *dq1 = taggedToDecQuad(tp1);
+    const decQuad *dq2 = taggedToDecQuad(tp2);
     decQuad d;
     decContext cx;
-    cx.status = 0;
-    cx.round = DEC_ROUND_HALF_EVEN;
+    initContext(&cx);
     decQuadAdd(&d, dq1, dq2, &cx);
-    result.panicCode = statusPanicCode(cx.status);
-    if (result.panicCode == 0) {
-        result.ptr = createDecimal(&d);
-    }
-    return result;    
+    return finish(&d, &cx);    
 }
 
 TaggedPtr _bal_decimal_const(const char *decString) {
