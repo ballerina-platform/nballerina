3ed2447318c4529c820c3c381d13c538e76a1146
Kavindu Gimhan Zoysa <kavindu175@gmail.com>
2021-11-13 23:36:12 +0530

    Adding Substraction op

--- runtime/decimal.c
+++ runtime/decimal.c
@@ -7,27 +7,12 @@ typedef GC decQuad *DecimalPtr;
 // XXX need to investigate DEC_Underflow
 #define DECIMAL_STATUS_FAIL (DEC_Errors|DEC_Subnormal)
 
+static TaggedPtrPanicCode finish(decQuad *dq, decContext *cx);
+
 static inline TaggedPtr createDecimal(decQuad *dq) {
     DecimalPtr dp = _bal_alloc(sizeof(decQuad));
     *dp = *dq;
     return ptrAddFlags(dp, (uint64_t)TAG_DECIMAL << TAG_SHIFT);
-}
-
-static inline TaggedPtrPanicCode finish(decQuad *dq, decContext *cx) {
-    TaggedPtrPanicCode result;
-    uint32_t status = cx->status;
-    if (status & DECIMAL_STATUS_FAIL) {
-        // XXX decide what panic codes to use 
-        result.panicCode = (status & DEC_Division_by_zero) ? PANIC_DIVIDE_BY_ZERO : PANIC_ARITHMETIC_OVERFLOW;
-    }
-    else {
-        result.panicCode = 0;
-        if (decQuadClass(dq) == DEC_CLASS_NEG_ZERO) {
-            decQuadZero(dq);
-        }
-        result.ptr = createDecimal(dq);
-    }
-    return result;  
 }
 
 static inline const decQuad *taggedToDecQuad(TaggedPtr tp) {
@@ -48,6 +33,33 @@ TaggedPtrPanicCode _bal_decimal_add(TaggedPtr tp1, TaggedPtr tp2) {
     initContext(&cx);
     decQuadAdd(&d, dq1, dq2, &cx);
     return finish(&d, &cx);    
+}
+
+TaggedPtrPanicCode _bal_decimal_sub(TaggedPtr tp1, TaggedPtr tp2) {
+    const decQuad *dq1 = taggedToDecQuad(tp1);
+    const decQuad *dq2 = taggedToDecQuad(tp2);
+    decQuad d;
+    decContext cx;
+    initContext(&cx);
+    decQuadSubtract(&d, dq1, dq2, &cx);
+    return finish(&d, &cx);    
+}
+
+TaggedPtrPanicCode finish(decQuad *dq, decContext *cx) {
+    TaggedPtrPanicCode result;
+    uint32_t status = cx->status;
+    if (status & DECIMAL_STATUS_FAIL) {
+        // XXX decide what panic codes to use 
+        result.panicCode = (status & DEC_Division_by_zero) ? PANIC_DIVIDE_BY_ZERO : PANIC_ARITHMETIC_OVERFLOW;
+    }
+    else {
+        result.panicCode = 0;
+        if (decQuadClass(dq) == DEC_CLASS_NEG_ZERO) {
+            decQuadZero(dq);
+        }
+        result.ptr = createDecimal(dq);
+    }
+    return result;  
 }
 
 TaggedPtr _bal_decimal_const(const char *decString) {
