9905a81fa9e3c81614ccf49407da7d778901893e
James Clark <jjc@jclark.com>
2022-02-07 15:58:09 +0700

    Fix names in runtime function tests

--- runtime/tests/inlinert_int.c
+++ runtime/tests/inlinert_int.c
@@ -6,13 +6,13 @@ void testIntNilCmp() {
     TaggedPtr b = _bal_int_to_tagged(IMMEDIATE_INT_MAX + 1);
     TaggedPtr c = _bal_int_to_tagged(IMMEDIATE_INT_MIN - 1);
     TaggedPtr nil = getNil();
-    assert(_bal_int_compare(a, nil) == COMPARE_UN);
-    assert(_bal_int_compare(b, nil) == COMPARE_UN);
-    assert(_bal_int_compare(c, nil) == COMPARE_UN);
-    assert(_bal_int_compare(nil, a) == COMPARE_UN);
-    assert(_bal_int_compare(nil, b) == COMPARE_UN);
-    assert(_bal_int_compare(nil, c) == COMPARE_UN);
-    assert(_bal_int_compare(nil, nil) == COMPARE_EQ);
+    assert(_bal_opt_int_compare(a, nil) == COMPARE_UN);
+    assert(_bal_opt_int_compare(b, nil) == COMPARE_UN);
+    assert(_bal_opt_int_compare(c, nil) == COMPARE_UN);
+    assert(_bal_opt_int_compare(nil, a) == COMPARE_UN);
+    assert(_bal_opt_int_compare(nil, b) == COMPARE_UN);
+    assert(_bal_opt_int_compare(nil, c) == COMPARE_UN);
+    assert(_bal_opt_int_compare(nil, nil) == COMPARE_EQ);
 }
 
 void testIntIntCmp() {
@@ -20,25 +20,25 @@ void testIntIntCmp() {
     TaggedPtr b = _bal_int_to_tagged(-5);
     TaggedPtr c = _bal_int_to_tagged(IMMEDIATE_INT_MAX + 1);
     TaggedPtr d = _bal_int_to_tagged(IMMEDIATE_INT_MIN - 1);
-    assert(_bal_int_compare(a, a) == COMPARE_EQ);
-    assert(_bal_int_compare(a, b) == COMPARE_GT);
-    assert(_bal_int_compare(a, c) == COMPARE_LT);
-    assert(_bal_int_compare(a, d) == COMPARE_GT);
+    assert(_bal_opt_int_compare(a, a) == COMPARE_EQ);
+    assert(_bal_opt_int_compare(a, b) == COMPARE_GT);
+    assert(_bal_opt_int_compare(a, c) == COMPARE_LT);
+    assert(_bal_opt_int_compare(a, d) == COMPARE_GT);
 
-    assert(_bal_int_compare(b, a) == COMPARE_LT);
-    assert(_bal_int_compare(b, b) == COMPARE_EQ);
-    assert(_bal_int_compare(b, c) == COMPARE_LT);
-    assert(_bal_int_compare(b, d) == COMPARE_GT);
+    assert(_bal_opt_int_compare(b, a) == COMPARE_LT);
+    assert(_bal_opt_int_compare(b, b) == COMPARE_EQ);
+    assert(_bal_opt_int_compare(b, c) == COMPARE_LT);
+    assert(_bal_opt_int_compare(b, d) == COMPARE_GT);
 
-    assert(_bal_int_compare(c, a) == COMPARE_GT);
-    assert(_bal_int_compare(c, b) == COMPARE_GT);
-    assert(_bal_int_compare(c, c) == COMPARE_EQ);
-    assert(_bal_int_compare(c, d) == COMPARE_GT);
+    assert(_bal_opt_int_compare(c, a) == COMPARE_GT);
+    assert(_bal_opt_int_compare(c, b) == COMPARE_GT);
+    assert(_bal_opt_int_compare(c, c) == COMPARE_EQ);
+    assert(_bal_opt_int_compare(c, d) == COMPARE_GT);
 
-    assert(_bal_int_compare(d, a) == COMPARE_LT);
-    assert(_bal_int_compare(d, b) == COMPARE_LT);
-    assert(_bal_int_compare(d, c) == COMPARE_LT);
-    assert(_bal_int_compare(d, d) == COMPARE_EQ);
+    assert(_bal_opt_int_compare(d, a) == COMPARE_LT);
+    assert(_bal_opt_int_compare(d, b) == COMPARE_LT);
+    assert(_bal_opt_int_compare(d, c) == COMPARE_LT);
+    assert(_bal_opt_int_compare(d, d) == COMPARE_EQ);
 }
 
 int main() {
--- runtime/tests/inlinert_string.c
+++ runtime/tests/inlinert_string.c
@@ -6,23 +6,23 @@ void testStringNilCmp() {
     TaggedPtr b = makeString("abd");
     TaggedPtr c = makeString("abb");
     TaggedPtr nil = getNil();
-    assert(_bal_string_compare(a, nil) == COMPARE_UN);
-    assert(_bal_string_compare(b, nil) == COMPARE_UN);
-    assert(_bal_string_compare(c, nil) == COMPARE_UN);
-    assert(_bal_string_compare(nil, a) == COMPARE_UN);
-    assert(_bal_string_compare(nil, b) == COMPARE_UN);
-    assert(_bal_string_compare(nil, c) == COMPARE_UN);
-    assert(_bal_string_compare(nil, nil) == COMPARE_EQ);
+    assert(_bal_opt_string_compare(a, nil) == COMPARE_UN);
+    assert(_bal_opt_string_compare(b, nil) == COMPARE_UN);
+    assert(_bal_opt_string_compare(c, nil) == COMPARE_UN);
+    assert(_bal_opt_string_compare(nil, a) == COMPARE_UN);
+    assert(_bal_opt_string_compare(nil, b) == COMPARE_UN);
+    assert(_bal_opt_string_compare(nil, c) == COMPARE_UN);
+    assert(_bal_opt_string_compare(nil, nil) == COMPARE_EQ);
 }
 
 void testStringStringCmp() {
     TaggedPtr a = makeString("abc");
     TaggedPtr b = makeString("abb");
-    assert(_bal_string_compare(a, a) == COMPARE_EQ);
-    assert(_bal_string_compare(a, b) == COMPARE_GT);
+    assert(_bal_opt_string_compare(a, a) == COMPARE_EQ);
+    assert(_bal_opt_string_compare(a, b) == COMPARE_GT);
 
-    assert(_bal_string_compare(b, a) == COMPARE_LT);
-    assert(_bal_string_compare(b, b) == COMPARE_EQ);
+    assert(_bal_opt_string_compare(b, a) == COMPARE_LT);
+    assert(_bal_opt_string_compare(b, b) == COMPARE_EQ);
 }
 
 HASH_DEFINE_KEY;
