9446ef23bf1bed1aff9232ffd227b3fb2d10a378
Heshan Padmasiri <heshanp@wso2.com>
2022-11-11 09:37:09 +0530

    fix comments

--- test/sub.mk
+++ test/sub.mk
@@ -1,14 +1,14 @@
-# TODO: update comment
 # The default target is `test`, which runs in 3 phases
 # 1. Remove the compile.stamp file if the compiler jar has changes
-# 2. Compile the .bal test cases into .ll files
-# 3. Compile, execute and check the output of .ll files
+# 2. If COMPILE_MODE is default: compile the .bal test cases into .ll files 
+# 	 Else: compile the .bal test cases into .o files (using the jni version of the compiler)
+# 3. Compile, execute and check the output of .ll/.o files
 # Phase 2 only updates .ll if they have changed.
 # Phases 2 and 3 use recursive invocations of make so dependencies are recalculated.
 # This is used in phases 1 and 2.
 # To run this, first navigate to out/<category> dir, then
-# You can do `make -f ../../sub.mk tdir=$(basename "$PWD") compile` to compile all changed test cases from .bal to .ll
-# You can do `make -f ../../sub.mk tdir=$(basename "$PWD") testll` to test the ll files.
+# You can do `make -f ../../sub.mk tdir=$(basename "$PWD") compile` to compile all changed test cases from .bal to .ll/.o files
+# You can do `make -f ../../sub.mk tdir=$(basename "$PWD") testIntermediate` to test the ll/o files.
 # Failing tests are listed in fail.txt
 COMPILER_JAR=../../../build/compiler/bin/nballerina.jar
 JNI_COMPILER_JAR=../../../testbuild/target/bin/nballerina.jar
@@ -33,7 +33,7 @@ RT=../../../runtime/balrt.a
 RT_INLINE=../../../runtime/balrt_inline.bc
 
 test: all
-	$(MAKE) -f ../../sub.mk tdir=$(tdir) testll
+	$(MAKE) -f ../../sub.mk tdir=$(tdir) testIntermediate
 
 all:
 ifeq ($(COMPILE_MODE),default)
@@ -66,8 +66,7 @@ compile.stamp: $(bal_files)
 endif
 
 # This compiles, runs and checks the output of ll/*.ll
-# TODO: different name (ie. without ll but we can't use test)
-testll: fail.txt $(expect_files) $(exe_files)
+testIntermediate: fail.txt $(expect_files) $(exe_files)
 
 fail.txt: $(diff_files)
 	@>$@
@@ -94,7 +93,7 @@ expect/%.txt: ../../../compiler/testSuite/$(tdir)/%.bal
 clean:
 	-rm -rf actual compile.stamp expect fail.txt ll result
 
-.PHONY: all test clean compile testll
+.PHONY: all test clean compile testIntermediate
 
 ifeq ($(COMPILE_MODE),default)
 .SECONDEXPANSION:
